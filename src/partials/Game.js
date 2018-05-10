f2df2d71b397ec8cae09af0ed5115358899fbd0d
Showwtime <jason.f.murphy@gmail.com>
2018-05-10 10:57:40 -0700

    set up board, paddles, ball, centerline

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,10 +1,13 @@
+import { SVG_NS } from "../settings";
+
 export default class Game {
 
 	constructor(element, width, height) {
 		this.element = element;
 		this.width = width;
 		this.height = height;
-	
+		
+		this.gameElement = document.getElementById(this.element);
 		// Other code goes here...
 	}
 
