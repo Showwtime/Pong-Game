6fd61c8eb340eceeac98e4b3c74401efa06f5eb2
Showwtime <jason.f.murphy@gmail.com>
2018-05-11 10:33:43 -0700

    added wall collision method and rebound

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -15,15 +15,35 @@ export default class Ball {
 reset() {
     this.x = this.boardWidth / 2;
     this.y = this.boardHeight / 2;
-    this.vy = Math.floor(Math.random() * 10 - 5);
+    // experiment with these values to change ball movement
+    this.vy = 0;
+    // try FOR loop instead of "while" for fun
+    while(this.vy === 0){
+        this.vy = Math.floor(Math.random() * 10 - 5);
+    }
     this.vx = this.direction * (6 - Math.abs(this.vy));    
-  }
+  } //end of reset
+
+  wallCollision(){
+    const hitLeft = this.x - this.radius <= 0;
+    const hitRight = this.x + this.radius >= this.boardWidth;
+    const hitTop = this.y - this.radius <= 0;
+    const hitBottom = this.y + this.radius >= this.boardHeight;
+
+    if(hitLeft || hitRight){
+        this.vx *= -1
+    }
+    if(hitTop || hitBottom){
+        this.vy *= -1
+    }
+  } //end of wall collision method
 
 
   render(svg, paddle1, paddle2) {
   
-    this.x += this.vx;
+    this.x += this.vx; 
     this.y += this.vy;
+
 
 
 
@@ -34,5 +54,7 @@ reset() {
     ball.setAttributeNS(null, "cy", this.y);
     ball.setAttributeNS(null, "fill", "white");
     svg.appendChild(ball);
-  }
-  }
+
+    this.wallCollision();
+  } //end of render
+  } //end of class Ball
